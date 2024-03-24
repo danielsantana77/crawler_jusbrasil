6db80b63e7c237ea9f3690f311e74243dec77ee1
Daniel Oliveira <30814811+danielsantana77@users.noreply.github.com>
2024-03-24 18:28:06 -0300

    fead : add first and second degree of Ceara courts functions

--- courts/tjce/first_degree_ce.py
+++ courts/tjce/first_degree_ce.py
@@ -1,3 +1,13 @@
 class FirstDegreeCeara:
-    def __init__(self):
-        self.url = "https://esaj.tjce.jus.br/cpopg/open.do"
+    def __init__(self, cnj):
+        self.cnj = cnj
+        self.state = "CE"
+        self.degree = "1 grau"
+        self.url_search_process = "https://esaj.tjce.jus.br/cpopg/search.do?"
+        self.url_show_process = "https://esaj.tjce.jus.br/cpopg/sshow.do?"
+
+    def get_url_show_process(self, process_code):
+        return self.url_show_process + f'processo.codigo={process_code}'
+
+    def get_url_search_process(self):
+        return self.url_search_process + f'cbPesquisa=NUMPROC&dePesquisa={self.cnj}'
--- courts/tjce/second_degree_ce.py
+++ courts/tjce/second_degree_ce.py
@@ -1,6 +1,13 @@
 class SecondDegreeCeara:
-    def __init__(self):
-        self.url_show = "https://esaj.tjce.jus.br/cposg5/show.do?"
+    def __init__(self, cnj):
+        self.cnj = cnj
+        self.state = "CE"
+        self.degree = "2 grau"
+        self.url_search_process = "https://esaj.tjce.jus.br/cposg5/search.do?"
+        self.url_show_process = "https://esaj.tjce.jus.br/cposg5/show.do?"
 
-    def get_data_link(self,process_code):
-        return self.url + f'processo.codigo={process_code}'
+    def get_url_show_process(self, process_code):
+        return self.url_show_process + f'processo.codigo={process_code}'
+
+    def get_url_search_process(self):
+        return self.url_search_process + f'cbPesquisa=NUMPROC&dePesquisa={self.cnj}'
