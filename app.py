ab89c5739a2fa3217ecd5f09cf4784ec873d9586
Daniel Oliveira <30814811+danielsantana77@users.noreply.github.com>
2024-03-25 15:27:12 -0300

    refactor : add Response method to response requests

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
-from flask import Flask, request
-from controller.controller import Controller
+import json
 
+from flask import Flask, request, Response
+from controller.controller import Controller
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def home():
-    return {'Message' : 'The server is running'}
+    message = {'Status': 'The server is running'}
+    return Response(response=json.dumps(message), mimetype='application/json', status=200)
 
-@app.route('/collect', methods=['POST'])
+
+@app.route('/api', methods=['GET'])
+def api():
+    message = {'Status': 'The api is connected'}
+    return Response(response=json.dumps(message), mimetype='application/json', status=200)
+
+
+@app.route('/api/collect', methods=['POST'])
 def collect():
     data = request.json
     if not 'processo' in data:
-        return {'Message': 'Error'}
+        message = {'Message': 'Invalid request '}
+        return Response(response=json.dumps(message), mimetype='application/json', status=400)
     cnj = data['processo']
     controller = Controller(cnj)
-    return controller.consult_process()
-
+    return Response(response=json.dumps(controller.consult_process()), mimetype='application/json', status=200)
 
 
 if __name__ == '__main__':
